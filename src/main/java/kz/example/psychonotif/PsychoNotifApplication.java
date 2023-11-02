a8ce20d95417c4ba6b2f0b8e9dc14d20fad5c8c7
Danya Zverev <i_am_zver@yahoo.com>
2023-11-02 16:29:00 +0600

    first commit

---
+++ src/main/java/kz/example/psychonotif/PsychoNotifApplication.java
@@ -0,0 +1,13 @@
+package kz.example.psychonotif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychoNotifApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsychoNotifApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/example/psychonotif/PsychoNotifApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.example.psychonotif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychoNotifApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
